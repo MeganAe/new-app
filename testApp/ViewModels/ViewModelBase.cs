7afa75135b198ea6a691d2d8e13f165a0aee55e9
MeganAe <ntkhang969@gmail.com>
2026-08-30 18:54:09 +0300

    test33

---
+++ testApp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace testApp.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ testApp/Views/MainView.axaml.cs
@@ -0,0 +1,13 @@
+
+using Avalonia;
+using Avalonia.Controls;
+
+namespace testApp.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
