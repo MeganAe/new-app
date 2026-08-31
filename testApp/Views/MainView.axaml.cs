e15303c30eed20f93315871e034ed99ba5e7dbbc
Metoushela Walker <168326500+MeganAe@users.noreply.github.com>
2026-08-31 08:16:06 +0300

    Implement StorageProvider wiring in MainView
    Add WireStorageProvider method to manage StorageProvider.

--- testApp/Views/MainView.axaml.cs
+++ testApp/Views/MainView.axaml.cs
@@ -1,6 +1,6 @@
-
 using Avalonia;
 using Avalonia.Controls;
+using testApp.ViewModels;
 
 namespace testApp.Views;
 
@@ -9,5 +9,15 @@ public partial class MainView : UserControl
     public MainView()
     {
         InitializeComponent();
+        AttachedToVisualTree += (_, _) => WireStorageProvider();
+        DataContextChanged += (_, _) => WireStorageProvider();
+    }
+
+    private void WireStorageProvider()
+    {
+        if (DataContext is MainViewModel vm)
+        {
+            vm.StorageProvider ??= TopLevel.GetTopLevel(this)?.StorageProvider;
+        }
     }
 }
